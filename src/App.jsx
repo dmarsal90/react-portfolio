09966a4aceeb86301476bb0424d4d6ea22c723c1
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-31 15:55:04 -0400

    Add Skills component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
-import { NavBar } from './assets/components/NavBar'
-import { Banner } from './assets/components/Banner'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {NavBar} from './assets/components/NavBar';
+import {Banner} from './assets/components/Banner';
+import {Skills} from "./assets/components/Skills";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <NavBar/>
-      <Banner/>
-    </div>
-  )
+    return (
+        <div className="App">
+            <NavBar/>
+            <Banner/>
+            <Skills/>
+        </div>
+    )
 }
 
 export default App

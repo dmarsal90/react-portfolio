0f92bc965dbbfb75040a34ee410deba80604e0a3
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-31 11:36:54 -0400

    Add NavBar to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { NavBar } from './assets/components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <NavBar/>
     </div>
   )
 }

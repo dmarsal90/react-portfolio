ef2364c9765d2ee46d7d67d94898c134114b9e98
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-01 11:05:11 -0400

    Add About to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {NavBar} from './assets/components/NavBar';
 import {Banner} from './assets/components/Banner';
+import {About} from "./assets/components/About";
 import {Skills} from "./assets/components/Skills";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <div className="App">
             <NavBar/>
             <Banner/>
+            <About/>
             <Skills/>
         </div>
     )

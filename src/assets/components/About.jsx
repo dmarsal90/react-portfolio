8a10da95370d9c6ed460854ca32da4ac19aa083b
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-01 15:58:22 -0400

    Update About component

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -1,7 +1,17 @@
-export const About = () =>{
-    return(
+export const About = () => {
+    return (
         <section className="about" id="about">
-            
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-bx wow zoomIn">
+                            <h2>About me</h2>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem
+                                Ipsum has been the industry's standard dummy text.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }

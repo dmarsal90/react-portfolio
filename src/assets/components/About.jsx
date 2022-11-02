450f8cea997d473abcf7980fd2896fdebb830c18
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-02 11:03:38 -0400

    Update About component

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -1,17 +1,160 @@
 export const About = () => {
-    return (
-        <section className="about" id="about">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="skill-bx wow zoomIn">
-                            <h2>About me</h2>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem
-                                Ipsum has been the industry's standard dummy text.</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="about section" id="about">
+      <div className="container">
+        <div className="row">
+          <div className="skill-bx wow zoomIn">
+            <div className="col-12">
+              <div class="section-title padd-15">
+                <h2>About me</h2>
+              </div>
             </div>
-        </section>
-    );
-}
+            <div class="row">
+              <div class="about-content padd-15">
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.<br></br> Lorem Ipsum has been the
+                  industry's standard dummy text.
+                </p>
+
+                <div className="row">
+                  <div className="education padd-15">
+                    <h3 className="title">Education</h3>
+                    <div className="row">
+                      <div className="timeline-box padd-15">
+                        <div className="timeline shadow-dark">
+                          <div className="timeline-item">
+                            <div className="circle-dot"></div>
+                            <h3 className="timeline-date">
+                              <i className="fa fa-calendar"></i>2014-2019
+                            </h3>
+                            <h4 className="timeline-title">
+                              Bachelor of Computer Science Engineer
+                            </h4>
+                            <h5 className="timeline-place">
+                              Ignacio Agramonte y Loynaz University
+                            </h5>
+                            <p>
+                              Lorem ipsum dolor sit, amet consectetur
+                              adipisicing elit. Aspernatur, quis assumenda, non,
+                              vero facilis distinctio consequuntur provident est
+                              quibusdam magnam nesciunt corporis molestias
+                              soluta ipsam voluptatum minima necessitatibus
+                              dolorum possimus.
+                            </p>
+                          </div>
+                          <div className="timeline-item">
+                            <div className="circle-dot"></div>
+                            <h3 className="timeline-date">
+                              <i className="fa fa-calendar"></i>2021
+                            </h3>
+                            <h4 className="timeline-title">
+                              Postgraduate in Cryptography
+                            </h4>
+                            <h5 className="timeline-place">UCI</h5>
+                            <p>
+                              Lorem ipsum dolor sit, amet consectetur
+                              adipisicing elit. Aspernatur, quis assumenda, non,
+                              vero facilis distinctio consequuntur provident est
+                              quibusdam magnam nesciunt corporis molestias
+                              soluta ipsam voluptatum minima necessitatibus
+                              dolorum possimus.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="row">
+                    <div className="experience padd-15">
+                      <h3 className="title">Experience</h3>
+                      <div className="row">
+                        <div className="timeline-box padd-15">
+                          <div className="timeline shadow-dark">
+                            <div className="timeline-item">
+                              <div className="circle-dot"></div>
+                              <h3 className="timeline-date">
+                                <i className="fa fa-calendar"></i>2017-2019
+                              </h3>
+                              <h4 className="timeline-title">
+                                Network administrator
+                              </h4>
+                              <h5 className="timeline-place">CITMA</h5>
+                              <p>
+                                Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Aspernatur, quis assumenda,
+                                non, vero facilis distinctio consequuntur
+                                provident est quibusdam magnam nesciunt corporis
+                                molestias soluta ipsam voluptatum minima
+                                necessitatibus dolorum possimus.
+                              </p>
+                            </div>
+                            <div className="timeline-item">
+                              <div className="circle-dot"></div>
+                              <h3 className="timeline-date">
+                                <i className="fa fa-calendar"></i>2019-2021
+                              </h3>
+                              <h4 className="timeline-title">
+                                Web Developer
+                              </h4>
+                              <h5 className="timeline-place">AHPC</h5>
+                              <p>
+                                Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Aspernatur, quis assumenda,
+                                non, vero facilis distinctio consequuntur
+                                provident est quibusdam magnam nesciunt corporis
+                                molestias soluta ipsam voluptatum minima
+                                necessitatibus dolorum possimus.
+                              </p>
+                            </div>
+                            <div className="timeline-item">
+                              <div className="circle-dot"></div>
+                              <h3 className="timeline-date">
+                                <i className="fa fa-calendar"></i>2021- feb 2022
+                              </h3>
+                              <h4 className="timeline-title">
+                              Web Developer
+                              </h4>
+                              <h5 className="timeline-place">CDAIf</h5>
+                              <p>
+                                Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Aspernatur, quis assumenda,
+                                non, vero facilis distinctio consequuntur
+                                provident est quibusdam magnam nesciunt corporis
+                                molestias soluta ipsam voluptatum minima
+                                necessitatibus dolorum possimus.
+                              </p>
+                            </div>
+                            <div className="timeline-item">
+                              <div className="circle-dot"></div>
+                              <h3 className="timeline-date">
+                                <i className="fa fa-calendar"></i>feb 2022 - Present
+                              </h3>
+                              <h4 className="timeline-title">
+                              Team leader
+                              </h4>
+                              <h5 className="timeline-place">CDAIf</h5>
+                              <p>
+                                Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Aspernatur, quis assumenda,
+                                non, vero facilis distinctio consequuntur
+                                provident est quibusdam magnam nesciunt corporis
+                                molestias soluta ipsam voluptatum minima
+                                necessitatibus dolorum possimus.
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
